d9cd9c265872b287d9a8b5c77e5525f363f4a4dd
joaquinmartinez2912 <jo.martinez@itecriocuarto.org.ar>
2024-04-17 00:26:53 -0300

    Challenge e instalacion bootstrap

---
+++ 2-Challange/Challange.js
@@ -0,0 +1,18 @@
+// Requisitos:
+// El usuario debe poder agregar nuevas tareas a la lista.
+// El usuario debe poder marcar las tareas como completadas.
+// El usuario debe poder eliminar tareas de la lista.
+// El usuario debe poder ver todas las tareas pendientes y las completadas.
+// Usar HTML Y css para adaptarlo a la web.
+// Pueden utilizar librerias por ejemplo bootstrap, etc.
+
+let listaToDo = document.getElementById("listaToDo");
+
+
+let crearTarea = () => {
+  e.preventDefault()
+  let item = document.createElement("li")
+  tarea = document.getElementById("tarea")
+
+  console.log(tarea)
+}
