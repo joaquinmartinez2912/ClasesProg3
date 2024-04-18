7af01c3ab5ac964198a09bdd0d1d65b0604889db
joaquinmartinez2912 <jo.martinez@itecriocuarto.org.ar>
2024-04-17 22:24:49 -0300

    agregar tareas y pruebas para eliminar y completar

--- 2-Challange/Challange.js
+++ 2-Challange/Challange.js
@@ -1,18 +1,49 @@
 // Requisitos:
-// El usuario debe poder agregar nuevas tareas a la lista.
-// El usuario debe poder marcar las tareas como completadas.
 // El usuario debe poder eliminar tareas de la lista.
+// El usuario debe poder marcar las tareas como completadas.
 // El usuario debe poder ver todas las tareas pendientes y las completadas.
 // Usar HTML Y css para adaptarlo a la web.
 // Pueden utilizar librerias por ejemplo bootstrap, etc.
 
-let listaToDo = document.getElementById("listaToDo");
+// El usuario debe poder agregar nuevas tareas a la lista.
+const listaToDoEnProceso = document.getElementById('listaToDoEnProceso')
+const listaToDoTerminadas = document.getElementById('listaToDoTerminadas')
+const formTarea = document.getElementById('formTarea') // Levanto todo lo que esta en el formulario.
 
-
-let crearTarea = () => {
+const agregarTarea = (e) => {
   e.preventDefault()
-  let item = document.createElement("li")
-  tarea = document.getElementById("tarea")
+  const tarea = formTarea.elements.tarea.value // "tarea" es el id del "textarea".
+  if (tarea !== '') { // Verificar si el textarea no está vacío
+    const item = document.createElement('li')
 
-  console.log(tarea)
+    const textoTarea = document.createTextNode(tarea)
+
+    const botonEliminar = document.createElement('button')
+    botonEliminar.textContent = 'Eliminar' 
+    botonEliminar.classList.add('btn', 'btn-danger', 'ml-5') // Agregar clases de Bootstrap al botón
+
+    // Agregar un evento click al botón para eliminar la tarea
+    botonEliminar.addEventListener('click', function () {
+      item.remove() 
+    })
+
+    const botorFinalizar = document.createElement('button')
+    botorFinalizar.textContent = 'Finalizar'
+    botorFinalizar.classList.add('btn', 'btn-success', 'ml-5')
+
+    botorFinalizar.addEventListener('click', function () {
+      item.removeChild(botorFinalizar)
+      listaToDoTerminadas.appendChild(item)
+    })
+
+    item.appendChild(textoTarea)
+    item.appendChild(botorFinalizar)
+    item.appendChild(botonEliminar)
+
+    listaToDoEnProceso.appendChild(item)
+    formTarea.reset()
+  }
 }
+
+formTarea.addEventListener('submit', agregarTarea) // Agrego la funcion al submit del formulario.
+
