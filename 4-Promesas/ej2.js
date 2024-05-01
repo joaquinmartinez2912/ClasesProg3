7b852c8347a489088d67d33af2aa2289bd1f37f0
joaquinmartinez2912 <jo.martinez@itecriocuarto.org.ar>
2024-05-01 13:01:14 -0300

    ejercicio2

--- 4-Promesas/ej2.js
+++ 4-Promesas/ej2.js
@@ -6,10 +6,55 @@
 //    calcular el precio total y confirmar la compra después de un breve período de espera, 
 //    todos estos datos los tengo que ver en una alerta.
 
-const productos = [
-    { id: 1, nombre: 'Laptop', precio: 1200, stock: 10 },
-    { id: 2, nombre: 'Teléfono móvil', precio: 800, stock: 0 },
-    { id: 3, nombre: 'Tablet', precio: 500, stock: 15 },
-    { id: 4, nombre: 'Smartwatch', precio: 300, stock: 30 },
-    { id: 5, nombre: 'Auriculares inalámbricos', precio: 150, stock: 0 }
-];
+const listaProductos = document.getElementById('listaProductos')
+
+// Para cargar las tareas que estan guardadas en localStorage.
+const cargarTareas = () => {
+    const listaOriginal =     [
+        { id: 1, nombre: 'Laptop', precio: 1200, stock: 10 },
+        { id: 2, nombre: 'Teléfono móvil', precio: 800, stock: 0 },
+        { id: 3, nombre: 'Tablet', precio: 500, stock: 15 },
+        { id: 4, nombre: 'Smartwatch', precio: 300, stock: 30 },
+        { id: 5, nombre: 'Auriculares inalámbricos', precio: 150, stock: 0 }
+    ];
+
+    let productos = localStorage.getItem('productos');  
+    console.log(productos)
+
+    if (productos == null) {
+        const listaOriginalJSON = JSON.stringify(listaOriginal);
+        localStorage.setItem('productos', listaOriginalJSON)
+    } 
+    
+    productos = localStorage.getItem('productos');  
+    const productosGuardados = JSON.parse(productos);
+    console.log(productos)
+    console.log(productosGuardados)
+
+    productosGuardados.forEach(producto => {
+      const itemProducto = crearItemProducto(producto.nombre)
+
+        listaProductos.appendChild(itemProducto)      
+    })
+  }
+  
+  const crearItemProducto = (nombre) => {
+    const item = document.createElement('li')
+  
+    const nombreProducto = document.createElement('p')
+    nombreProducto.textContent = nombre
+    
+    const botonComprar = document.createElement('button')
+    botonComprar.textContent = 'Comprar'
+    botonComprar.classList.add('btn', 'btn-success', 'ml-5')
+    botonComprar.addEventListener('click', () => {
+        // Aca iria la logica del proceso de compra
+    })
+    
+    item.appendChild(nombreProducto)
+    item.appendChild(botonComprar)
+  
+    return item
+  }
+
+  window.addEventListener('load', cargarTareas)
